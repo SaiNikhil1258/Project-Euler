6d03d7ec6469e3586aae8521cee4a1dd36e9813c
Sai Nikhil <111006116+SaiNikhil1258@users.noreply.github.com>
2024-03-13 23:48:04 +0530

    Update p40.py

--- p40.py
+++ p40.py
@@ -7,7 +7,7 @@
 # If dn represents the nth digit of the fractional part, find the value of the following expression.
 
 # d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
-
+import unittest
 
 
 def number(n):
